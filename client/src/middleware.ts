984b40a510a3acbbc55531386ec780e04c7b2711
thverse <thion.art@gmail.com>
2025-03-11 21:19:08 +0900

    [CLIENT] - 미들웨어 수정중

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,9 +1,39 @@
 import { NextRequest, NextResponse } from "next/server";
 
+const publicPaths = ["/signup", "/signin"];
+const protectedPaths = ["/main"];
+
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/signin", request.url));
+  const { pathname } = request.nextUrl;
+  const hasTokens =
+    request.cookies.get("accessToken") || request.cookies.get("refreshToken");
+  const isAuthenticated = hasTokens;
+
+  // 미들웨어 검열이 필요없는 경로들
+  if (
+    pathname.startsWith("/_next") ||
+    pathname.startsWith("/api") ||
+    pathname.includes(".")
+  ) {
+    return NextResponse.next();
+  }
+
+  // 인증된 사용자는 모든 URL 접근가능
+  if (isAuthenticated && publicPaths.includes(pathname)) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  // 미인증된 사용자는 로그인 페이지로
+  if (
+    !isAuthenticated &&
+    protectedPaths.some((path) => pathname.startsWith(path))
+  ) {
+    return NextResponse.redirect(new URL("/signin", request.url));
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: "/about",
+  matcher: "/((?!_next/static|_next/image|favicon.ico|public/).*)",
 };
