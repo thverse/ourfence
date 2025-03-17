f745287f00a211c70e67830bda0c3f9f874d7de8
thverse <thion.art@gmail.com>
2025-03-18 00:00:42 +0900

    [CLIENT] -이미지 최적화중

--- client/src/components/layout/Post.tsx
+++ client/src/components/layout/Post.tsx
@@ -1,7 +1,37 @@
+import Image from "next/image";
+
 const Post = () => {
   return (
     <div className="p-4 border-y-[1px] border-gray-200">
-      <span>Post</span>
+      {/* POST CONTENT */}
+      <div className="flex gap-4">
+        {/* AVATAR */}
+        <div className="relative w-10 h-10 rounded-full overflow-hidden">
+          <Image
+            src="/avatar.png"
+            width={100}
+            height={100}
+            alt="User avatar"
+            priority
+          />
+        </div>
+        {/* CONTENT */}
+        <div className="flex-1">
+          {/* TOP */}
+          <div className="flex">
+            <div className="flex items-center gap-2 flex-wrap">
+              <span className="text-base font-bold">best1m</span>
+              <span>1 day ago</span>
+            </div>
+          </div>
+          {/* TEXT & MEDIA */}
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+          </p>
+          <Image src="/testbg.jpg" width={500} height={500} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
