83b0538bf269cc931d8add7156a161f4e5f7e4f7
thverse <thion.art@gmail.com>
2025-03-20 18:27:58 +0900

    [CLIENT] - 피드에서 다른 페이지로 라우팅할때 탭이 고정되는 버그 수정

--- client/src/components/layout/MainContent.tsx
+++ client/src/components/layout/MainContent.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import TopBar from "./TopBar";
 import Feed from "@/modules/feed/components/Feed";
+import { useTopBarStore } from "@/app/store";
 
 const MainContent = () => {
+  const { setSelectTopBarItem } = useTopBarStore();
+  useEffect(() => {
+    return () => {
+      setSelectTopBarItem("myPosts"); // 다른곳으로 라우팅 될때 TopBar 상태값 초기화
+    };
+  }, [setSelectTopBarItem]);
   return (
     <>
       <TopBar />
