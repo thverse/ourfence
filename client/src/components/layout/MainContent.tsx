c949f3eabeb4a7b6922bbd8d1d855e05248ce45c
thverse <thion.art@gmail.com>
2025-03-26 14:53:59 +0900

    [CLIENT] - 탭바 피드안으로 이동

--- client/src/components/layout/MainContent.tsx
+++ client/src/components/layout/MainContent.tsx
@@ -4,23 +4,8 @@ import Feed from "@/modules/feed/components/Feed";
 import { useTabBarStore } from "@/app/store";
 
 const MainContent = () => {
-  const tabBarItems = [
-    { id: "myPosts", label: "내 게시물" },
-    { id: "followingsPosts", label: "팔로잉 게시물" },
-  ];
-  const { setSelectedTabId } = useTabBarStore();
-  useEffect(() => {
-    return () => {
-      setSelectedTabId("myPosts"); // 다른곳으로 라우팅 될때 TabBar 상태값 초기화
-    };
-  }, [setSelectedTabId]);
   return (
     <>
-      <TabBar
-        items={tabBarItems}
-        initialActiveTab={tabBarItems[0].id}
-        className="top-0 sticky"
-      />
       <Feed />
     </>
   );
--- client/src/modules/feed/components/Feed.tsx
+++ client/src/modules/feed/components/Feed.tsx
@@ -3,23 +3,37 @@
 import { useTabBarStore } from "@/app/store";
 import Post from "../../post/components/Post";
 import { useEffect } from "react";
+import TabBar from "@/components/TabBar";
 
 const Feed = () => {
-  const { selectedTabId } = useTabBarStore();
+  const { selectedTabId, setSelectedTabId } = useTabBarStore();
+  console.log(selectedTabId);
+  const tabBarItems = [
+    { id: "myPosts", label: "내 게시물" },
+    { id: "followingsPosts", label: "팔로잉 게시물" },
+  ];
 
   useEffect(() => {
+    if (!selectedTabId || selectedTabId !== "myPosts") {
+      setSelectedTabId("myPosts");
+    }
     window.scrollTo(0, 0);
-  }, [selectedTabId]);
+  }, []);
 
-  if (selectedTabId !== "myPosts") {
-    return null;
-  }
+  // if (selectedTabId !== "myPosts") return null;
 
   return (
-    <div className="flex flex-col gap-4">
-      <Post />
-      <Post />
-      <Post />
+    <div>
+      <TabBar
+        items={tabBarItems}
+        initialActiveTab={tabBarItems[0].id}
+        className="top-0 sticky"
+      />
+      <div className="flex flex-col gap-4">
+        <Post />
+        <Post />
+        <Post />
+      </div>
     </div>
   );
 };
