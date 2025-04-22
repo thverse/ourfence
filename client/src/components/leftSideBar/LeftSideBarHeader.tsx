37af1db9d303343d252cd3e221216eda831995b1
thverse <thion.art@gmail.com>
2025-04-22 12:32:51 +0900

    [CLIENT] - 왼쪽 사이드바 헤더 수정

--- client/src/components/leftSideBar/LeftSideBarHeader.tsx
+++ client/src/components/leftSideBar/LeftSideBarHeader.tsx
@@ -1,40 +1,14 @@
-import React from "react";
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
-import Link from "next/link";
-import { useCurrentUser } from "@/modules/user/hooks/useUser";
-const user = {
-  name: "기마무개",
-  username: "kim",
-  profileImage: "/tb.png",
-};
+import { useRouter } from "next/navigation";
 
 const LeftSideBarHeader = () => {
-  const { data: user } = useCurrentUser();
+  const router = useRouter();
 
   return (
-    <div className="flex justify-center items-center flex-col border-b-2">
-      <div className="text-3xl font-bold pb-10">Ourfence</div>
-      <div className="flex flex-col justify-center items-center">
-        <Avatar className="w-20 h-20">
-          <AvatarImage src={""} alt={user?.username} className="rounded-full" />
-          <AvatarFallback>{user?.username[0]}</AvatarFallback>
-        </Avatar>
-        <div className="font-bold pt-2">{user?.username}</div>
-      </div>
-      <div className="flex justify-between items-center pt-5 pb-5 w-full">
-        <Link href="/" className="flex flex-col items-center">
-          <div className="font-bold text-xl">1</div>
-          <div>posts</div>
-        </Link>
-        <Link href="/followers" className="flex flex-col items-center">
-          <div className="font-bold text-xl">1</div>
-          <div>followers</div>
-        </Link>
-        <Link href="/following" className="flex flex-col items-center">
-          <div className="font-bold text-xl">1</div>
-          <div>following</div>
-        </Link>
-      </div>
+    <div
+      className="flex justify-center items-center flex-col border-b-2 cursor-pointer"
+      onClick={() => router.push("/")}
+    >
+      <div className="text-3xl font-bold pb-1">Ourfence</div>
     </div>
   );
 };
