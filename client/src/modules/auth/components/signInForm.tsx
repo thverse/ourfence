349d749637d2155ebccd75925d551292bc82566d
thverse <thion.art@gmail.com>
2025-05-15 22:41:15 +0900

    [CLIENT] - 구글 로그인 주소 수정

--- client/src/modules/auth/components/signInForm.tsx
+++ client/src/modules/auth/components/signInForm.tsx
@@ -37,7 +37,7 @@ export function SignInForm({
   });
 
   const onGoogleSignIn = () => {
-    window.location.href = `http://localhost:4000/api/google-signin`;
+    window.location.href = `http://${process.env.NEXT_PUBLIC_API_URL}/api/google-signin`;
   };
 
   return (
